8ef0c3af33affecaeefa0c55f91f175c7ee22183
Ravi Sharma <ruravi80@gmail.com>
2022-09-06 21:35:10 +0530

    fix validation

--- validation/student.validate.js
+++ validation/student.validate.js
@@ -1,30 +1,33 @@
-const joi = require('joi')
+const joi = require("joi");
 
 const studentValidationSchema = joi.object({
-    studentData: joi
-      .object({
-        name: joi
-          .string()
-          .pattern(/^[A-Za-z ]+$/)
-          .required(),
-        age: joi
-          .string()
-          .pattern(/^[0-9]{2}$/)
-          .required(),
-        email: joi
-          .string()
-          .pattern(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          )
-          .required(),
-        phone: joi
-          .string()
-          .length(10)
-          .pattern(/^[0-9]+$/)
-          .required(),
-        address: joi.string().required(),
-      })
-      .required(),
-  });
+  studentData: joi
+    .object({
+      name: joi
+        .string()
+        .pattern(/^[A-Za-z ]+$/)
+        .required(),
+      age: joi
+        .string()
+        .pattern(/^[0-9]{2}$/)
+        .required(),
+      email: joi
+        .string()
+        .pattern(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )
+        .required(),
+      phone: joi
+        .string()
+        .length(10)
+        .pattern(/^[0-9]+$/)
+        .required(),
+      address: joi
+        .string()
+        // .pattern(/^[a-zA-Z0-9\s,.'"- ]*$/)
+        .required(),
+    })
+    .required(),
+});
 
-module.exports = studentValidationSchema
+module.exports = studentValidationSchema;
